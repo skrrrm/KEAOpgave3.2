6bbab429e5cf3a9e04cf3199386d5586d2d59561
skrrrm <a@macbook-windows>
2020-02-21 09:07:37 +0100

    Add project files.

---
+++ KEAOpgave3.2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace KEAOpgave3._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClick = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelRes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonClick
+            // 
+            this.buttonClick.Location = new System.Drawing.Point(413, 176);
+            this.buttonClick.Name = "buttonClick";
+            this.buttonClick.Size = new System.Drawing.Size(188, 45);
+            this.buttonClick.TabIndex = 0;
+            this.buttonClick.Text = "Click me!";
+            this.buttonClick.UseVisualStyleBackColor = true;
+            this.buttonClick.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(160, 78);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(143, 25);
+            this.labelInfo.TabIndex = 1;
+            this.labelInfo.Text = "Type a name!";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(364, 71);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(237, 31);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // labelRes
+            // 
+            this.labelRes.AutoSize = true;
+            this.labelRes.Location = new System.Drawing.Point(165, 263);
+            this.labelRes.Name = "labelRes";
+            this.labelRes.Size = new System.Drawing.Size(0, 25);
+            this.labelRes.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelRes);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.buttonClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClick;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelRes;
+    }
+}
+
---
+++ KEAOpgave3.2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Design et lille program, hvor brugeren indtaster navn. 
+ * Hvis navnet et ”Børge” skrives ”Øv ud Børge”.
+ * Ellers skrives ”Velkommen <indtastet navn>”
+ */
+namespace KEAOpgave3._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Variables
+        string name;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            name = textBoxName.Text.ToLower();
+            if (name == "børge")
+                labelRes.Text = "Øv ud Børge";
+            else
+                labelRes.Text = $"Velkommen {name}";
+        }
+    }
+}
